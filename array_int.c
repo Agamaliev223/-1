bbb4d8c1dc687e044bba43f0fac6eaeab7fc1763
Agamaliev223 <145326583+Agamaliev223@users.noreply.github.com>
2024-12-03 19:05:50 +0500

    Update array_int.c

--- array_int.c
+++ array_int.c
@@ -3,6 +3,10 @@
 
 bool array_set(struct SArrayInt *a, size_t index, int64_t value)
 {
+    if (!a)
+    {
+        return false;
+    }
     if (index < a->size)
     {
         a->data[index] = value;
@@ -13,6 +17,10 @@ bool array_set(struct SArrayInt *a, size_t index, int64_t value)
 
 struct SValidInt64 array_get(struct SArrayInt *a, size_t index)
 {
+    if (!a)
+    {
+        return none_int64;
+    }
     if (index < a->size)
     {
         return some_int64(a->data[index]);
